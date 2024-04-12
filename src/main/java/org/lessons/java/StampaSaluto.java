728337e413716915107837230cdaaa81b697d665
andreaacomelli <comelliandrea21@gmail.com>
2024-04-12 11:18:00 +0200

    primo commit

---
+++ src/main/java/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,11 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
